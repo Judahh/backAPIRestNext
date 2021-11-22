f5f47f4adcb9f8496c522436783c966223fe9449
Judahh <judahholanda7@gmail.com>
2021-11-22 09:13:03 -0300

    add useStep

--- source/router/stepIndex.ts
+++ source/router/stepIndex.ts
@@ -8,36 +8,51 @@ import baseRouter from './baseRouter';
 console.log('Initializing Routes...');
 let dBHandler;
 let done = false;
+const useStep = process.env.USE_STEP
+  ? process.env.USE_STEP.toLowerCase() === 'true' ||
+    process.env.USE_STEP === '1'
+  : false;
 
 const stepIndex = (
   routerSingleton: RouterCreator,
   databaseHandler: DatabaseHandler
 ): RouterSingleton => {
-  if (dBHandler === undefined) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    dBHandler = databaseHandler;
-  } else if (!done) {
-    try {
-      const stepSize = process.env.STEP_SIZE ? +process.env.STEP_SIZE : 1000;
-      Promise.race([
-        timer(stepSize),
-        createRoutes(
-          routerSingleton,
-          dBHandler.getInit(),
-          dBHandler.getMauth()
-        ),
-      ]).then((finished: boolean) => {
-        done = finished;
-        console.log('finished:', finished);
-      });
-    } catch (error) {
-      console.log('error:', error);
-      return baseRouter(500, error);
+  if (useStep) {
+    if (dBHandler === undefined) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      dBHandler = databaseHandler;
+    } else if (!done) {
+      try {
+        const stepSize = process.env.STEP_SIZE ? +process.env.STEP_SIZE : 1000;
+        Promise.race([
+          timer(stepSize),
+          createRoutes(
+            routerSingleton,
+            dBHandler.getInit(),
+            dBHandler.getMauth()
+          ),
+        ]).then((finished: boolean) => {
+          done = finished;
+          console.log('finished:', finished);
+        });
+      } catch (error) {
+        console.log('error:', error);
+        return baseRouter(500, error);
+      }
+    } else {
+      return routerSingleton.getInstance() as RouterSingleton;
     }
+    return baseRouter(503, 'Server still initializing, please try later');
   } else {
+    if (dBHandler === undefined) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      dBHandler = databaseHandler;
+    }
+    if (!done) {
+      createRoutes(routerSingleton, dBHandler.getInit(), dBHandler.getMauth());
+    }
     return routerSingleton.getInstance() as RouterSingleton;
   }
-  return baseRouter(503, 'Server still initializing, please try later');
 };
 
 export default stepIndex;
