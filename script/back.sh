f91c45c96f4b9d36d85a265fde97898106f04501
Restyled.io <commits@restyled.io>
2022-06-21 20:07:48 +0000

    Restyled by shfmt

--- script/back.sh
+++ script/back.sh
@@ -1,19 +1,18 @@
 #! /bin/bash
-while getopts p:db flag
-do
-    case "$flag" in
-        p) port=$OPTARG;;
-        d) exec="dev";;
-        b) exec="build";;
-    esac
+while getopts p:db flag; do
+  case "$flag" in
+    p) port=$OPTARG ;;
+    d) exec="dev" ;;
+    b) exec="build" ;;
+  esac
 done
 
 pwd=$PWD
-cd "$pwd";
+cd "$pwd"
 
 if [ -f ".env" ]; then
-    echo ".env exists."
-    export "$(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )"
+  echo ".env exists."
+  export "$(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}')"
 fi
 
 port="${PORT:=3000}"
@@ -22,11 +21,14 @@ echo "setted Port: $port"
 
 case $exec in
   "dev")
-    node_modules/next/dist/bin/next -p "$port";;
+    node_modules/next/dist/bin/next -p "$port"
+    ;;
 
   "build")
-    node_modules/next/dist/bin/next build;;
+    node_modules/next/dist/bin/next build
+    ;;
 
   "")
-    node_modules/next/dist/bin/next start;;
+    node_modules/next/dist/bin/next start
+    ;;
 esac
