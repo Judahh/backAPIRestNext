b26e6801519ec86f58368da8d9977a6fa06eb61c
Restyled.io <commits@restyled.io>
2022-06-22 12:28:25 +0000

    Restyled by shellharden

--- script/back.sh
+++ script/back.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 while getopts p:db flag
 do
-    case "${flag}" in
-        p) port=${OPTARG};;
+    case "$flag" in
+        p) port=$OPTARG;;
         d) exec="dev";;
         b) exec="build";;
     esac
 done
 
-pwd=$(pwd)
-cd $pwd;
+pwd=$PWD
+cd "$pwd";
 
 if [ -f ".env" ]; then
     echo ".env exists."
     cat .env | grep -v '#' | grep PORT
-    export $(cat .env | grep -v '#' | grep PORT)
+    export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 port="${PORT:=3000}"
 
-echo "setted Port: ${port}"
+echo "setted Port: $port"
 
 case $exec in
   "dev")
-    node_modules/next/dist/bin/next -p $port;;
+    node_modules/next/dist/bin/next -p "$port";;
 
   "build")
     node_modules/next/dist/bin/next build;;
