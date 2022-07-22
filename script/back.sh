5c479411e308073ee73033a0c3b238b6dfefa592
Restyled.io <commits@restyled.io>
2022-07-22 14:18:13 +0000

    Restyled by shfmt

--- script/back.sh
+++ script/back.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
-while getopts p:db flag
-do
-    case "$flag" in
-        p) port=$OPTARG;;
-        d) exec="dev";;
-        m) exec="migrate";;
-        b) exec="build";;
-    esac
+while getopts p:db flag; do
+  case "$flag" in
+    p) port=$OPTARG ;;
+    d) exec="dev" ;;
+    m) exec="migrate" ;;
+    b) exec="build" ;;
+  esac
 done
 
 dist="${npm_package_config_path_dist:-.}"
@@ -14,12 +13,12 @@ server="${npm_package_config_path_server:-source/server.js}"
 file=$dist/$server
 
 pwd=$PWD
-cd "$pwd";
+cd "$pwd"
 
 if [ -f ".env" ]; then
-    echo ".env exists."
-    cat .env | grep -v '#' | grep PORT
-    export "$(cat .env | grep -v '#' | grep PORT)"
+  echo ".env exists."
+  cat .env | grep -v '#' | grep PORT
+  export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 port="${PORT:=3000}"
@@ -28,18 +27,22 @@ echo "setted Port: $port"
 
 case $exec in
   "dev")
-    node_modules/next/dist/bin/next -p "$port";;
+    node_modules/next/dist/bin/next -p "$port"
+    ;;
 
   "build")
-    node_modules/next/dist/bin/next build;;
+    node_modules/next/dist/bin/next build
+    ;;
 
   "migrate")
     if test -f "$file"; then
       (node "$file" -m)
     else
       (node ./node_modules/@backapirest/express/script/migrate.mjs -f "$pwd")
-    fi;;
+    fi
+    ;;
 
   "")
-    node_modules/next/dist/bin/next start;;
+    node_modules/next/dist/bin/next start
+    ;;
 esac
