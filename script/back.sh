3f753bd74f975ffe9b2116218d3f7dc757ce1f3f
Restyled.io <commits@restyled.io>
2022-07-25 20:02:22 +0000

    Restyled by shellharden

--- script/back.sh
+++ script/back.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 while getopts p:dmb flag
 do
-    case "${flag}" in
-        p) port=${OPTARG};;
+    case "$flag" in
+        p) port=$OPTARG;;
         d) exec="dev";;
         m) exec="migrate";;
         b) exec="build";;
@@ -13,31 +13,31 @@ dist="${npm_package_config_path_dist:-.}"
 server="${npm_package_config_path_server:-source/server.js}"
 file=$dist/$server
 
-pwd=$(pwd)
-cd $pwd;
+pwd=$PWD
+cd "$pwd";
 
 if [ -f ".env" ]; then
     echo ".env exists."
     cat .env | grep -v '#' | grep PORT
-    export $(cat .env | grep -v '#' | grep PORT)
+    export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 port="${PORT:=3000}"
 
-echo "setted Port: ${port}"
+echo "setted Port: $port"
 
 case $exec in
   "dev")
-    node_modules/next/dist/bin/next -p $port;;
+    node_modules/next/dist/bin/next -p "$port";;
 
   "build")
     node_modules/next/dist/bin/next build;;
 
   "migrate")
     if test -f "$file"; then
-      (node $file -m)
+      (node "$file" -m)
     else
-      (npm tsc & node ./node_modules/@backapirest/next/script/migrate.mjs -f $pwd)
+      (npm tsc & node ./node_modules/@backapirest/next/script/migrate.mjs -f "$pwd")
     fi;;
 
   "")
